616ac8abb2e473b90865db366c44634239af53f4
Valeria Piont <iceflame.vvv@gmail.com>
2022-02-22 17:17:09 +0300

    done: resolved Account REST API task

--- account-rest-api/src/main/java/com/bobocode/web/controller/AccountRestController.java
+++ account-rest-api/src/main/java/com/bobocode/web/controller/AccountRestController.java
@@ -1,21 +1,60 @@
 package com.bobocode.web.controller;
 
 import com.bobocode.dao.AccountDao;
+import com.bobocode.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * <p>
- * todo: 1. Configure rest controller that handles requests with url "/accounts"
- * todo: 2. Inject {@link AccountDao} implementation
- * todo: 3. Implement method that handles GET request and returns a list of accounts
- * todo: 4. Implement method that handles GET request with id as path variable and returns account by id
- * todo: 5. Implement method that handles POST request, receives account as request body, saves account and returns it
- * todo:    Configure HTTP response status code 201 - CREATED
- * todo: 6. Implement method that handles PUT request with id as path variable and receives account as request body.
- * todo:    It check if account id and path variable are the same and throws {@link IllegalStateException} otherwise.
- * todo:    Then it saves received account. Configure HTTP response status code 204 - NO CONTENT
- * todo: 7. Implement method that handles DELETE request with id as path variable removes an account by id
- * todo:    Configure HTTP response status code 204 - NO CONTENT
- */
+import java.util.List;
+import java.util.Objects;
+
+@RestController("/accounts")
 public class AccountRestController {
+
+    private AccountDao accountDao;
+
+    @Autowired
+    public AccountRestController(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    // Implement method that handles GET request and returns a list of accounts
+    @GetMapping
+    public List<Account> getAllAccounts(){
+        return accountDao.findAll();
+    }
+
+    // Implement method that handles GET request with id as path variable and returns account by id
+    @GetMapping("/{id}")
+    public Account getAccountById(@PathVariable("id") Long id){
+        return accountDao.findById(id);
+    }
+
+
+    // Implement method that handles POST request, receives account as request body, saves account and returns it
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Account saveAccount(@RequestBody Account account){
+        accountDao.save(account);
+        return account;
+    }
+
+    // Implement method that handles PUT request with id as path variable and receives account as request body.
+    @PutMapping("{/id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateAccount(@PathVariable("id") Long id, @RequestBody Account account){
+        if(Objects.equals(account.getId(), id)){
+            throw new IllegalArgumentException("Account id and path variable are the same");
+        }
+        accountDao.save(account);
+    }
+
+    // Implement method that handles DELETE request with id as path variable removes an account by id
+    @DeleteMapping("{/id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAccount(@PathVariable("id") Long id){
+        accountDao.remove(accountDao.findById(id));
+    }
 
 }
